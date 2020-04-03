6fb9bef01dcc268775b721af0090ab59fd82b756
shivam <shivam.g17@iiits.in>
2020-04-03 15:27:07 +0530

    change

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Host(models.Model):
+    name=models.CharField(max_length =100, default='')
+    email_address=models.EmailField(max_length = 254) 
+    phone=models.BigIntegerField()
+    address=models.CharField(max_length=100,default='', blank=True)
+
+    def __str__(self):
+        return self.name
