8c1ec4990ff72bb05bb8a96e82aed8a45f887b35
shivam <shivam.g17@iiits.in>
2020-04-08 23:08:04 +0530

    physio

--- physiotherapist/views.py
+++ physiotherapist/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
+    user = request.user
+    profile = Physiotherapist.objects.get(user=user)
+    context={
+           'profile':profile
+    }
     
-    return render(request, 'physiotherapist/physiotherapist.html')
+    return render(request, 'physiotherapist/physiotherapist.html',context)
