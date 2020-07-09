e24dfb399a2d51887254ba01bf7895e6280ec29e
shivam <shivam.gupta@innovaccer.com>
2020-07-09 23:24:04 +0530

    Complete Nurse

--- nurse/models.py
+++ nurse/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from patient.models import Patient
+from datetime import date
+from datetime import datetime
+from django.db.models.signals import pre_save
+from django.urls import reverse
 
 # Create your models here.
 class Nurse(models.Model):
@@ -12,9 +17,8 @@ class Nurse(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
-    speciality = models.CharField(max_length=20)
     experience = models.IntegerField()
-    profile_pic = models.ImageField(default='nurse.jpeg', upload_to='Nurse_pics', blank=True)
+    profile_photo=models.ImageField(upload_to='media_/nuse_profile_pic/')
     house_no = models.CharField(max_length=10)
     city = models.CharField(max_length=20)
     state = models.CharField(max_length=20)
@@ -26,3 +30,69 @@ class Nurse(models.Model):
     
     def __str__(self):
         return f'{self.first_name}'
+
+class Slot1(models.Model):
+    time_start = models.DateTimeField()
+    time_end = models.DateTimeField()
+    def __str__(self):
+        return f'{self.time_start} to {self.time_end}'
+
+# class AppointmentPhysio(models.Model):
+#     STATUS_CHOICES = (
+#         ('U', 'Upcoming'),
+#         ('C', 'Completed'),
+#     )
+#     # slot = models.OneToOneField(Slot, on_delete=models.CASCADE)
+#     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+#     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+#     nurse = models.ForeignKey(Nurse, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return f'Patient:{self.patient}, Physio:{self.nurse}'   
+
+class BookingDate(models.Model):
+    nurse=models.ForeignKey(Nurse,on_delete=models.CASCADE, null=True,blank=True)
+    date=models.DateField(default=datetime.now)
+
+    def __str__(self):
+        return str(self.date)
+    def get_absolute_url(self):
+        return reverse('nurse:create_slot',kwargs={'pk':self.pk})
+
+class Slot(models.Model):
+    TIME_CHOICES = (('09:00:00', '9 am'),
+                    ('10:00:00', '10 am'),
+                    ('11:00:00', '11 am'),
+                    ('12:00:00', '12 pm'),
+                    ('13:00:00', '1 pm'),
+                    ('14:00:00', '2 pm'),
+                    ('15:00:00', '3 pm'),
+                    ('16:00:00', '4 pm'),
+                    ('17:00:00', '5 pm'),
+                    ('18:00:00', '6 pm'), )
+    nurse=models.ForeignKey(Nurse,on_delete=models.CASCADE, null=True,blank=True)
+    date=models.ForeignKey(BookingDate,on_delete=models.CASCADE, null=True,blank=True)
+    start_time=models.CharField(max_length=200,choices=TIME_CHOICES,null=True,blank=True)
+    slot_status=models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('nurse','start_time','date')
+
+    def __str__(self):
+    	return str(self.start_time)
+
+class Nurse_complaint_feedback(models.Model):
+    COMPLAINT_CHOICES = (
+        ('SALARY', 'SALARY'),
+        ('BOOKING', 'BOOKING'),
+        ('OTHER/FEEDBACK','OTHER/FEEDBACK')
+    )
+    STATUS_CHOICES = (
+        ('SENT TO ADMIN', 'SENT TO ADMIN'),
+        ('RESOLVED', 'RESOLVED'),
+    )
+
+    nurse=models.ForeignKey(Nurse,on_delete=models.CASCADE, null=True,blank=True)
+    specify_type=models.CharField(max_length=200,choices=COMPLAINT_CHOICES,null=False,blank=False)
+    date=models.DateField(auto_now_add=True)
+    status=models.CharField(max_length=200,choices=STATUS_CHOICES,default='SENT TO ADMIN')
+    description=models.TextField(null=False, blank=False)
--- nurse/views.py
+++ nurse/views.py
@@ -1,5 +1,259 @@
 from django.shortcuts import render
+from .models import Nurse, Slot
+from django.contrib.auth.models import User
+from .forms import SlotForm
+from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+import datetime
+from django.urls import reverse,reverse_lazy
+from django.db.models import Max
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.db import IntegrityError
+from django.utils.decorators import method_decorator
+from .forms import Add_Profile, Modify_Profile
+from .models import BookingDate,Slot, Nurse_complaint_feedback
+from django.http import HttpResponseRedirect
+from django.utils import timezone
+
+
+def physio_home(request):
+    user = request.user
+    profile = Nurse.objects.get(user=user)
+    fb= Nurse_complaint_feedback.objects.all()
+    # print(fb)
+    context={
+           'profile':profile
+    }
+    
+    return render(request, 'nurse/nurse.html',context)
 
 # Create your views here.
-def home(request):
-    return render(request, 'nurse/nurse.html')
+# def home(request):
+#     username = request.user.username
+#     user_instance = User.objects.get(username=username)
+#     physio_instance = Nurse.objects.get(user=user_instance)
+#     upcoming_appointments = AppointmentPhysio.objects.filter(nurse=physio_instance, status='U')
+#     completed_appointments = AppointmentPhysio.objects.filter(nurse=physio_instance, status='C')
+#     if request.method == 'GET':
+#         slot_form = SlotForm()
+#         # slot_form.fields['time_start'].widget = DateTimePickerInput()
+#     elif request.method == 'POST':
+#         slot_form = SlotForm(request.POST)
+#         if slot_form.is_valid():
+#             slot_form.save()    
+#     return render(request, 'nurse/nurse1.html',{
+#         'upcoming': upcoming_appointments,
+#         'completed': completed_appointments,
+#         'slot_form': slot_form,
+#     })
+
+
+
+
+def verification(request):
+    return render(request,'nurse/verification.html')
+
+def index(request):
+     #print(user)
+
+    return render(request,'nurse/profile_home.html')
+
+
+def make_profile(request):
+    user = request.user
+    if request.method=="POST":
+        form=Add_Profile(request.POST, request.FILES ,initial={'user':user,'email_id':user.email})
+
+        if form.is_valid():
+            profile_item=form.save(commit=False)
+            profile_item.user = user
+            profile_item.verified=False
+            profile_item.email_id=user.email
+            profile_item.save()
+
+            # return render(request, "nurse/verification.html", {})
+            return redirect('/nurse/home/')
+
+
+    else:
+
+        form=Add_Profile(initial={'user':user,'email_id':user.email})
+        #form.fields['user'].widget.attrs['disabled'] = True
+        #form.fields['user'].editable=False
+    return render(request,'nurse/new.html',{'form':form})
+
+
+def modify_profile(request):
+    user = request.user
+    profile_item = Nurse.objects.get(user=user)
+    form=Modify_Profile(request.POST or None, instance=profile_item)
+    if form.is_valid():
+            form.save()
+            return redirect('/nurse/home/')
+    return render(request,'nurse/new.html',{'form':form})
+
+
+def Show_Profile(request):
+        user = request.user
+        profile = Nurse.objects.get(user=user)
+        context={
+            'profile':profile
+        }
+        if(profile.verified==True):
+            # print('&&')
+            return render(request,'nurse/show_profile.html',context)
+        else:
+            # print('%%')
+            return redirect(reverse('nurse:verification'))
+
+def create_slot(request, pk):
+    user=request.user
+    form = SlotForm(request.POST or None)
+    date = get_object_or_404(BookingDate, pk=pk)
+    if form.is_valid():
+
+        item = form.save(commit=False)
+        item.date = date
+        profile = Nurse.objects.get(user=user)
+        item.nurse =profile
+        try:
+            item.save()
+        except IntegrityError as e:
+            context = {
+                'date': date,
+                'form': form,
+                'message':"*Slot already Exists"
+            }
+            return render(request, 'nurse/create_slot.html', context)
+
+        return redirect('/nurse/home/')
+    context = {
+        'date': date,
+        'form': form,
+    }
+
+    return render(request, 'nurse/create_slot.html', context)
+
+# @method_decorator(login_url=reverse_lazy('login'))
+class DateCreate(CreateView):
+
+    model=BookingDate
+    fields=['date',]
+
+
+    def get_initial(self):
+
+         max_date=BookingDate.objects.all().aggregate(Max('date'))
+        #  print(max_date)
+        #  print(datetime)
+         if max_date['date__max'] == None:
+              max_date['date__max'] = timezone.now() + datetime.timedelta(days=1)
+        #  print(max_date)     
+         key, value = max_date.popitem()
+        #  value += datetime.timedelta(days=1)
+        #  print(value)
+        # value=int(list(max_id.values())[0])
+        # value=value+1
+        # #user = request.user
+        #
+        #
+        # #print(value)
+         initial = super(DateCreate, self).get_initial()
+         initial.update({'date': value})
+         return initial
+    def form_valid(self, form):
+        user=self.request.user
+
+        profile = Nurse.objects.get(user=user)
+        date = form.save(commit=False)
+        # print(date)
+        try:
+            obj = BookingDate.objects.filter(date=str(date)).filter(nurse=profile).first()
+        except BookingDate.DoesNotExist:
+            obj = None
+        # obj=get_object_or_404(BookingDate, date=str(date))
+        # print('SHIVAM')
+        # print(obj)
+        #print(obj.pk)
+        if(obj==None):
+            # print("no")
+            date.nurse = profile
+            return super(DateCreate, self).form_valid(form)
+        else:
+            # print('Yes')
+            #return create_slot(self.request,pk=obj.pk-1)
+            return redirect(str(obj.pk) + '/slot/')
+            #return reverse('doctor_profile:create_slot',kwargs={'pk':int(obj.pk)})
+            #return HttpResponse('OK')
+            #return super(DateCreate, self).form_invalid(form)
+        # context={
+        #
+		#     "object":appointment,
+		# }
+		# return render(self.request,'booking/booking_confirmation.html', context=context)
+
+
+# @login_required(login_url=reverse_lazy('login'))
+def show_slots(request):
+    user=request.user
+    profile = Nurse.objects.get(user=user)
+    first_name=profile.first_name
+    last_name=profile.last_name
+    dates=Slot.objects.filter(nurse=profile).order_by('start_time')
+    print(dates)
+    return render(request,'nurse/show_slots.html',{'slots':dates,'first_name':first_name,'last_name':last_name})
+
+class ComplaintFeedbackCreate(CreateView):
+
+    model= Nurse_complaint_feedback
+    fields=['specify_type','description']
+
+
+    def form_valid(self, form):
+        user=self.request.user
+
+        profile = Nurse.objects.get(user=user)
+        feedback = form.save(commit=False)
+        feedback.nurse = profile
+        feedback.save()
+        return redirect('/nurse/home/')
+        # print(date)
+        # try:
+        #     obj = BookingDate.objects.filter(date=str(date)).filter(nurse=profile).first()
+        # except BookingDate.DoesNotExist:
+        #     obj = None
+        # # obj=get_object_or_404(BookingDate, date=str(date))
+        # print('SHIVAM')
+        # print(obj)
+        # #print(obj.pk)
+        # if(obj==None):
+        #     print("no")
+        #     date.nurse = profile
+        #     return super(DateCreate, self).form_valid(form)
+        # else:
+        #     print('Yes')
+        #     #return create_slot(self.request,pk=obj.pk-1)
+        #     return redirect(str(obj.pk) + '/slots/')
+            #return reverse('doctor_profile:create_slot',kwargs={'pk':int(obj.pk)})
+            #return HttpResponse('OK')
+            #return super(DateCreate, self).form_invalid(form)
+        # context={
+        #
+		#     "object":appointment,
+		# }
+		# return render(self.request,'booking/booking_confirmation.html', context=context)
+def show_complaint_feedback(request):
+    user=request.user
+    profile = Nurse.objects.get(user=user)
+    first_name=profile.first_name
+    last_name=profile.last_name
+    feedback=Nurse_complaint_feedback.objects.filter(nurse=profile)
+    # print(feedback)
+    return render(request,'nurse/show_feedback.html',{'feedbacks':feedback,'first_name':first_name,'last_name':last_name})
+
+def delete_slot(request, slot_id):
+    # print(request.get['slot_id'])
+    Slot.objects.filter(pk=slot_id).delete()
+    return redirect('/nurse/slots/')
