f7e482888c947cb737f2c14715ffaebc748260d5
Kalpajyoti <kalpahe@gmail.com>
2020-07-24 12:09:22 +0530

    completed_collector

--- samplecollector/urls.py
+++ samplecollector/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
     path('modify_profile/',views.modify_profile,name='collector_modify_profile'),
     path('assigned_samples/',views.samples_to_be_collected, name='collector_assigned_samples'),
     path('collected_samples/',views.collected_samples, name='collector_collected_samples'),
+    url(r'^confirm_collection/(?P<booking_id>\w+)/$',views.confirm_collection,name='confirm_collection'),
     url(r'^collection_details/(?P<booking_id>\w+)/$',views.collection_details,name='collector_collection_details'),
-    
+    url(r'^collector_collected_details/(?P<booking_id>\w+)/$',views.collector_collected_details,name='collector_collected_details'),
     
     
     ]
